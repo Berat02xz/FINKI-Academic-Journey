4455a9ded7dd08c799074d4ba46257c63c465e84
Berat Ahmetaj <beratahmetaj02@gmail.com>
2022-02-27 12:17:33 +0100

    Labs1 Finished

--- OOP/Labs1/2.c
+++ OOP/Labs1/2.c
@@ -23,15 +23,70 @@ typedef struct otsecka{
      tocka t2;
 }otsecka;
 
-
 //методи
-void se_secat(otsecka o1,otsecka o2){
-    printf("%f", o1.t1.x );
+int max(int a, int b){
+    if(a>=b){
+        return a;
+    } else {
+        return b;
+    }
 }
+
+int min(int a, int b){
+    if(a>=b){
+        return b;
+    } else {
+        return a;
+    }
+}
+
+
+int onLine(otsecka l1, tocka p) {   //check whether p is on the line or not
+   if(p.x <= max(l1.t1.x, l1.t2.x) && p.x <= min(l1.t1.x, l1.t2.x) &&
+      (p.y <= max(l1.t1.y, l1.t2.y) && p.y <= min(l1.t1.y, l1.t2.y)))
+      return 1;
+   
+   return 0;
+} 
+
+int direction(tocka a, tocka b, tocka c) {
+   int val = (b.y-a.y)*(c.x-b.x)-(b.x-a.x)*(c.y-b.y);
+   if (val == 0){
+      return 0; }   //colinear
+   else if(val < 0){
+      return 2; }  //anti-clockwise direction
+      return 1;    //clockwise direction
+}
+
+int isIntersect(otsecka l1,otsecka l2) {
+   //four direction for two lines and points of other line
+   int dir1 = direction(l1.t1, l1.t2, l2.t1);
+   int dir2 = direction(l1.t1, l1.t2, l2.t2);
+   int dir3 = direction(l2.t1, l2.t2, l1.t1);
+   int dir4 = direction(l2.t1, l2.t2, l1.t2);
+   
+   if(dir1 != dir2 && dir3 != dir4) {
+      return 1; //they are intersecting
+   }
+   if(dir1==0 && onLine(l1, l2.t1)) //when p2 of line2 are on the line1
+    {  return 1; }
+
+   if(dir2==0 && onLine(l1, l2.t2)) //when p1 of line2 are on the line1
+    {  return 1; }
+
+   if(dir3==0 && onLine(l2, l1.t1)) //when p2 of line1 are on the line2
+     { return 1; }
+
+   if(dir4==0 && onLine(l2, l1.t2)) //when p1 of line1 are on the line2
+     { return 1;}
+         
+   return 0;
+}
+
 
 //главна програма
 int main() {
-    double x1, y1, x2, y2;
+    float x1, y1, x2, y2;
     scanf("%f %f %f %f", &x1, &y1, &x2, &y2);
     tocka t1 = { x1, y1 };
     tocka t2 = { x2, y2 };
@@ -41,8 +96,8 @@ int main() {
     tocka t3 = { x1, y1 };
     tocka t4 = { x2, y2 };
     otsecka o2 = { t3, t4 };
-    printf("direktno : %f \n", o1.t1.x );
-    se_secat(o1, o2);
+    
+    printf("%d",isIntersect(o1, o2) );
 
     return 0;
 }
--- OOP/Labs1/c3.c
+++ OOP/Labs1/c3.c
@@ -57,17 +57,21 @@ if (AB+BC == AC){
 
 int main() {
     float x1, y1, x2, y2;
+    printf("Vnesete x, y:\n");
     scanf("%f %f", &x1, &y1);
+    printf("Vnesete x2, y2:\n");
 	scanf("%f %f", &x2, &y2);
 	tocka2D t1 = { x1, y1 };
 	tocka2D t2 = { x2, y2 };
-	printf("%.2f\n", rastojanie(t1, t2));
+	printf("Rastojanieto pomegu tie dve tocki: %.2f\n", rastojanie(t1, t2));
     float z1, z2;
+
+    printf("Vnesete x, y:\n");
     scanf("%f %f", &z1, &z2);
     tocka3D t3 = {x1, y1, z1};
     tocka3D t4 = {x2, y2, z2};
-    printf("%.2f\n", rastojanie3D(t3, t4));
+    printf("Rastojanie vo 3D: %.2f\n", rastojanie3D(t3, t4));
     tocka2D t5 = {z1, z2};
-    printf("%d\n", ista_prava(t1, t2, t5));
+    printf("Ista Prava: %d\n", ista_prava(t1, t2, t5));
 	return 0;
 }
