c03022dfab6a9655bae4f57e89f915519751a81c
Berat Ahmetaj <beratahmetaj02@gmail.com>
2023-10-15 20:09:23 +0200

    WP_aud1

---
+++ Web_Programming/wp-auds/src/main/java/mk/ukim/finki/wpauds/WpAudsApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wpauds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpAudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpAudsApplication.class, args);
+    }
+
+}
---
+++ Web_Programming/wp-auds/src/test/java/mk/ukim/finki/wpauds/WpAudsApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wpauds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpAudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
