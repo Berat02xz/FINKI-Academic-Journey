1c4ee68dab1a100422770bdcb4dd89d8fa3b88e2
BeratAhmetaj <beratahmetaj02@gmail.com>
2024-03-13 16:54:57 +0100

    fresh start

--- IS - Integrated Systems/AUD/EShop/EShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
