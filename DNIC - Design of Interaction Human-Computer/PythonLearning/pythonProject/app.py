15b72be5f7a3a8ef8739ebbe3e593c02ef145f5d
BeratAhmetaj <beratahmetaj02@gmail.com>
2024-03-13 16:22:23 +0100

    MUSIC Model

--- DNIC - Design of Interaction Human-Computer/PythonLearning/pythonProject/app.py
+++
@@ -1,7 +0,0 @@
-character_name = "Geroge"
-character_age = 70
-
-print("There once was a man named " + character_name + ",")
-print("he was " + str(character_age) + " years old. ")
-print("He really liked the name "+character_name+", ")
-print("but didnt like being "+ str(character_age) +"")
