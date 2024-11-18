dbac8f01e56f26a0e8def6387c16cb4f65966b9f
BeratAhmetaj <beratahmetaj02@gmail.com>
2024-11-18 17:55:32 +0100

    Зголемете го бројот на коцки на таблата во 8х8, и сменете ги сите соодветни вредности за да се задржи правилен приказ на таблата.

--- PNVI - Video Game Programming/Slide Puzzle/SlidePuzzle.py
+++ PNVI - Video Game Programming/Slide Puzzle/SlidePuzzle.py
@@ -7,8 +7,8 @@ import pygame, sys, random
 from pygame.locals import *
 
 # Create the constants (go ahead and experiment with different values)
-BOARDWIDTH = 4  # number of columns in the board
-BOARDHEIGHT = 4 # number of rows in the board
+BOARDWIDTH = 8  # number of columns in the board
+BOARDHEIGHT = 8 # number of rows in the board
 TILESIZE = 90
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
