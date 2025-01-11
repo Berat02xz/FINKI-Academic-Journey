ccb5b05d72c89cb422d1fe253a07298bb9ba9f51
BeratAhmetaj <beratahmetaj02@gmail.com>
2025-01-11 16:09:51 +0100

    MIS

--- MIS - Mobile information systems/Lab_3/functions/node_modules/firebase-functions-test/lib/cloudevent/mocks/firestore/firestore-on-document-updated-with-auth-context.d.ts
+++
@@ -1,4 +0,0 @@
-import { MockCloudEventAbstractFactory } from '../../types';
-import { Change, firestore } from 'firebase-functions/v2';
-import { QueryDocumentSnapshot } from 'firebase-admin/firestore';
-export declare const firestoreOnDocumentUpdatedWithAuthContext: MockCloudEventAbstractFactory<firestore.FirestoreAuthEvent<Change<QueryDocumentSnapshot>>>;
